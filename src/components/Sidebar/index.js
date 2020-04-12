0ede7e89aed6a7dfcd359e5e732dcf4b395a0530
Simms <simms1993@hotmail.co.uk>
2020-04-13 00:08:16 +0100

    Updated sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,8 +9,40 @@ import Card from '../UI/Card';
 const Sidebar = (props) => {
     return (
         <div className="sidebarContainer">
-            <Card   >
-               <div className = "cardHeader"></div>
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}  >
+                <div className="cardHeader">
+                    <span>About Us</span>
+                </div>
+                <div className="profileImageContainer">
+                    <img src={require('../../blogPostImages/profileImage.jpg')} alt="Post Image" />
+                </div>
+                <div className="cardBody">
+                    <p className="personalBio">My name is Alsahid Simms. I am a software developer</p>
+                </div>
+            </Card>
+
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }} >
+                <div className="cardHeader">
+                    <span>Social Network</span>
+                </div>
+            </Card>
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className="cardHeader">
+                    <span>Recent Posts</span>
+                </div>
+
+                <div className="recentPosts">
+                    <div className="recentPost">
+                        <h3>Post Title</h3>
+                        <span>March 11,2020</span>
+                    </div>
+
+                    <div className="recentPost">
+                        <h3>Post Title</h3>
+                        <span>March 11,2020</span>
+                    </div>
+                </div>
+
             </Card>
         </div>
 
