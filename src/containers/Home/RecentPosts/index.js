5014ece3196ecd447e06398987ca991b6c2eb570
alsahid <simms1993@hotmail.co.uk>
2020-05-18 20:14:16 +0100

    Modified sub side images

---
+++ src/containers/Home/RecentPosts/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './style.css';
+import Card from '../../../components/UI/Card';
+
+/**
+* @author
+* @function RecentPosts
+**/
+
+const RecentPosts = (props) => {
+    return (
+        <div style={props.style}>
+            <Card style={{ marginBottom: '20px' }}>
+                <div className="phostImageWrapper">
+                <img src={require('../../../blogPostImages/bridge.jpg')} alt="Post" />
+                </div>
+
+                <div style={{ textAlign: 'center' }}>
+                    <span>Featured</span>
+                    <h2>Fitness Manatra To Live Fit Life</h2>
+                    <span>Posted on Jan 20 2020 by Keith Blogging'R us</span>
+                    <p>Passage its ten led hearted removal cordial. Preference any astonished unreserved mrs. Prosperous understood middletons in conviction an uncommonly do.
+                    Supposing so be resolving breakfast am or perfectly. Is drew am hill from mr. Valley by oh twenty direct me so. Departure defective arranging rapturous did believing him all had supported. Family months lasted simple set nature vulgar him. Picture for attempt joy excited ten carried manners talking how.
+               Suspicion neglected he resolving agreement perceived at an. </p>
+
+                    <button>Read More</button>
+                </div>
+
+            </Card>
+
+
+        </div>
+    )
+
+}
+
+export default RecentPosts
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import './style.css';
 import Card from '../../components/UI/Card';
+import Sidebar from '../../components/Sidebar';
+import RecentPosts from './RecentPosts';
+
+const SideImage = props =>{
+  return(
+  <div style= {{height: `${props.sideImageHeight}px`}}>
+    <img src={props.src} alt="" />
+    </div>
+  );
+}
+
+const ImageGallery = props => (
+  <div className= "galleryPost" style= {props.galleryStyle}>
+    <section style = {{width: props.largeWidth}}>
+      <div className = "mainImageWrapper">
+      <img src = {require('../../blogPostImages' + props.imagesArray[1])} alt=""/>
+      </div>
+    </section>
+    <section className = {"sideImageWrapper"} style = {{width: props.smallWidth}}>
+      {
+        props.imagesArray.map(image =>
+          <SideImage
+          height = {props.sideImageHeight}
+          src = {require('../../blogPostImages/'+ image)}
+          alt = ""/>
+          )
+      }
+    </section>
+  </div>
+);
 
 
 const Home = props => {
@@ -15,34 +45,36 @@ const Home = props => {
 
   return (
     <div>
-      <Card style = {{margin:'20px 0'}}>
+      <Card >
         <div className="galleryPost" style={galleryStyle}>
+
           <section style={{ width: '70%' }}>
             <div>
               <img src={require('../../blogPostImages/flight.jpg')} alt="Post" />
             </div>
           </section>
           <section className="sideImageWrapper" style={{ width: '30%' }}>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img src={require('../../blogPostImages/atlanta.jpg')} alt="Post" />
-            </div>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img src={require('../../blogPostImages/miami.jpg')} alt="Post" />
-            </div>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img src={require('../../blogPostImages/police.jpg')} alt="Post" />
-            </div>
+           <SideImage 
+           height = {sideImageHeight}
+            src={require('../../blogPostImages/atlanta.jpg')} alt=""
+             />
+           <SideImage 
+           height = {sideImageHeight}
+            src={require('../../blogPostImages/miami.jpg')} alt=""
+             />
+           <SideImage 
+           height = {sideImageHeight}
+            src={require('../../blogPostImages/police.jpg')} alt=""
+             />
           </section>
         </div>
       </Card>
 
-      <Card style = {{margin:'20px 0'}}>
-        Post 1
-        </Card>
-      <Card style =  {{margin:'20px 0'}}>
-        Post 1
-        </Card>
+      <section className="HomeContainer">
 
+        <RecentPosts style= {{ width: '70%' }} />
+        <Sidebar />
+      </section>
     </div>
   );
 }
